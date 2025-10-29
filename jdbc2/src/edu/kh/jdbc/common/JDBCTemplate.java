73ad315f8b9dfb5a1556994d86cb5b4c7eff8d03
Ads1213 <ads11121316@gmail.com>
2025-10-29 15:34:49 +0900

    2025-10-29 JDBC 2일차

--- jdbc2/src/edu/kh/jdbc/common/JDBCTemplate.java
+++ jdbc2/src/edu/kh/jdbc/common/JDBCTemplate.java
@@ -3,6 +3,8 @@ package edu.kh.jdbc.common;
 import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Properties;
 
 /* JDBCTemplate
@@ -20,6 +22,9 @@ import java.util.Properties;
  * 객체로 만들지 않고도 메서드를 사용할 수 있도록 하기 위해
  * 모든 메서드를 public static 으로 선언
  * 
+ * 
+ */
+/**
  * 
  */
 public class JDBCTemplate {
@@ -69,11 +74,93 @@ public class JDBCTemplate {
 		
 	}
 	
-	public static void commit() {
+	
+	/** 전달 받은 커넥션에서 수행한 SQL을 commit 하는 메서드
+	 * 
+	 */
+	public static void commit(Connection conn) {
 		
+		try {
+			// conn 생성되지 않거나 닫혀있지 않을 때 commit
+			if(conn != null && !conn.isClosed()) conn.commit();
+			
+		} catch (Exception e) {
+			System.out.println("커밋 중 예외 발생");
+			e.printStackTrace();
+		}
 	}
 	
 	
+	/** 전달 받은 커넥션에서 수행한 SQL을 rollback 하는 메서드
+	 * @param conn
+	 */
+	public static void rollback(Connection conn) {
+		
+		try {
+			// 커넥션이 null이 아니고 닫혀있지도 않으면 커넥션을 통해 롤백해주도록 하겠다.
+			if(conn != null && !conn.isClosed()) conn.rollback();
+			
+		} catch (Exception e) {
+			System.out.println("롤백 중 예외발생");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	// ------------------------
+	
+	// Connection, Statement(PreparedStatement), ResultSet
+	
+	
+	/** 전달받은 커넥션을 close(자원반환)하는 메서드
+	 * 
+	 */
+	public static void close(Connection conn) {
+		
+		try {
+			if(conn != null && !conn.isClosed()) conn.close();
+			
+			
+		} catch (Exception e) {
+			System.out.println("커넥션 close() 중 예외 발생");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	// 오버로딩이란?
+	// 과적 - 같은 이름의 메서드 과적
+	// 조건 : 매개변수, 타입, 순서가 달라야 한다.
+	/** 전달 받은 Statement or PreparedStatement 둘 다 close() 할 수 있는 메서드
+	 * + 다형성의 업캐스팅 적용
+	 * -> PreparedStatement는 Statement의 자식
+	 */
+	public static void close(Statement stmt) {
+		
+		try {
+			if(stmt != null && !stmt.isClosed()) stmt.close();
+			
+		} catch (Exception e) {
+			System.out.println("Statement close() 중 예외 발생");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/** 전달 받은 ResultSet을 close() 하는 메서드
+	 * @param rs
+	 */
+	public static void close(ResultSet rs) {
+		
+		try {
+			if(rs != null && !rs.isClosed()) rs.close();
+			
+		} catch (Exception e) {
+			System.out.println("ResultSet close() 중 예외발생");
+			e.printStackTrace();
+		}
+		
+	}
 	
 	
 	
